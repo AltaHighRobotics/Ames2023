68fcac3f1a60c38cac5a908cb80c6ab51dc61b21
Vichathon Bunnarith <vbunnarithschool@gmail.com>
2023-12-01 16:31:23 -0700

    Got rid of useless thingy

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrainSub;
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants;
 
 public class AutoCommand extends CommandBase {
